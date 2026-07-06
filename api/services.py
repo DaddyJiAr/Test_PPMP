6887fe4a963f3b927b2533635797293dbd6ecee7
DaddyJiAr <santiagojr0212@gmail.com>
2026-07-06 19:38:03 +0800

    testing

--- api/services.py
+++ api/services.py
@@ -4,6 +4,10 @@ from supabase import create_client
 
 load_dotenv()
 
+print("SUPABASE_URL =", os.getenv("SUPABASE_URL"))
+print("ANON exists =", os.getenv("SUPABASE_ANON_KEY") is not None)
+print("SERVICE exists =", os.getenv("SUPABASE_SERVICE_ROLE_KEY") is not None)
+
 url = os.getenv("SUPABASE_URL")
 
 public_supabase = create_client(url, os.getenv("SUPABASE_ANON_KEY"))
