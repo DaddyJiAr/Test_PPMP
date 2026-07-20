0562e5734799b386665e153888a1a290a1c49260
DaddyJiAr <santiagojr0212@gmail.com>
2026-07-20 16:51:01 +0800

    eme

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('header_info/', views.get_header_info),
     path('admin_name/', views.get_admin_name),
-    path('signatories/', views.get_signatories),
     path('update_fullname/', views.update_fullname),
 ]
--- user/views.py
+++ user/views.py
@@ -22,23 +22,6 @@ def get_admin_name(request):
         admin_name = private_supabase.table("USER").select("FullName").eq("Role", "Admin").single().execute()
         return Response({"fullname": admin_name.data["FullName"]}, status=200)
 
-@api_view(['POST'])
-def get_signatories(request):
-    user = get_user(request)
-    if user is None:
-        return Response({"error": "User not found"}, status=401)
-    document_type = request.POST["documentType"]
-    if document_type is None:
-        return Response({"error": "Document type not found"}, status=401)
-    response = private_supabase.table("DOCUMENT_SIGNATORY").select("*").eq("DocumentType", document_type.upper()).execute()
-    if response is None:
-        return Response({"error": "Document type not found"}, status=401)
-    signatories = [{
-        "fullName": signatory["FullName"],
-        "position": signatory["PositionTitle"],
-    }for signatory in response.data]
-    return Response({"signatories": signatories}, status=200)
-
 
 @api_view(['PUT'])
 def update_fullname(request):
